c14bd36d10a4032e10314ef2d2364780be7bb40f
Xiburse <50038191+Xiburse@users.noreply.github.com>
2021-03-23 18:20:22 +0800

    none

--- src/renderer/components/ReadBook/ContentsChild.vue
+++ src/renderer/components/ReadBook/ContentsChild.vue
@@ -3,13 +3,11 @@
         <div @click.left="contentTitleClickLeft"
              class="contentTitle">{{list.name}}</div>
         <div class="listBox">
-            <transition-group name="childListTran">
-                <contents-child v-for="l in childList"
-                                :list="l"
-                                :level="level + 1"
-                                :key="l.id"
-                                class="childListClass"></contents-child>
-            </transition-group>
+            <contents-child v-for="l in list.navPoint"
+                            :list="l"
+                            :level="level + 1"
+                            :key="l.id"
+                            class="childListClass"></contents-child>
         </div>
     </div>
 </template>
@@ -25,44 +23,61 @@ export default {
             }
         }
     },
-    props: {
-        list: Object,
-        level: Number
-    },
     watch: {
         ifListBox: function () {
             if (this.ifListBox) {
-                var i = 0
-                var s = 0
-                s = setInterval(() => {
-                    if (i == this.list.navPoint.length) {
-                        clearInterval(s)
-                    } else {
-                        this.childList.push(this.list.navPoint[i])
-                        i++
-                    }
-                }, 50)
-            } else if (!this.ifListBox) {
-                var s = 0
-                s = setInterval(() => {
-                    if (this.childList.length == 0) {
-                        clearInterval(s)
-                    } else {
-                        this.childList.splice(this.childList.length - 1, 1)
-                    }
-                }, 50)
+                var listBoxEle = this.listBoxEle
+                var first = listBoxEle.getBoundingClientRect()
+
+                listBoxEle.style.height = "auto"
+                var last = listBoxEle.getBoundingClientRect()
+
+                listBoxEle.style.height = "0px"
+                requestAnimationFrame(function () {
+                    var l = last
+                    var lbe = listBoxEle
+
+                    lbe.style.transition = "0.4s"
+                    lbe.style.height = l.height + "px"
+                })
+            } else {
+                var lbe = this.listBoxEle
+                requestAnimationFrame(function () {
+                    var listBoxEle = lbe
+                    listBoxEle.style.transition = "0.4s"
+                    listBoxEle.style.height = "0px"
+                })
+                
             }
         }
+    },
+    props: {
+        list: Object,
+        level: Number
     },
     data: function () {
         return {
             isHaveChild: false,
             ifListBox: false,
 
-            childList: []
+            listBoxEle: ""
         }
     },
-    mounted: function () { },
+    mounted: function () {
+        var nodeList = this.$el.children
+        for (var i = 0; i < nodeList.length; i++) {
+            if (nodeList[i].className == "listBox") {
+                this.listBoxEle = nodeList[i]
+                break
+            }
+        }
+
+        var lbe = this.listBoxEle
+        this.listBoxEle.addEventListener("transitionend", function () {
+            var listBoxEle = lbe
+            listBoxEle.style.transition = ""
+        })
+    },
     created: function () {
         if (this.list.navPoint.length == 0) {
             this.isHaveChild = false
@@ -81,12 +96,8 @@ export default {
     transition: 0.4s;
 }
 
-.childListTran-enter,
-.childListTran-leave-to {
-    opacity: 0;
-    transform: translateY(30px);
-}
-.childListTran-enter-active, .childListTran-leave-active {
-    position: absolute;
+.listBox {
+    overflow: hidden;
+    height: 0px;
 }
 </style>
